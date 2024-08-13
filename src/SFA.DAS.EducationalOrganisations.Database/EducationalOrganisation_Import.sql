1d619d53b5dd180d6d7b009cba51643845852c83
Alan Burns <adburns57@gmail.com>
2024-08-13 15:38:33 +0100

    Change database project from C# to SQL Server Database Project type

---
+++ src/SFA.DAS.EducationalOrganisations.Database/EducationalOrganisation_Import.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE dbo.[EducationalOrganisation_Import] (
+    [Id]                uniqueidentifier	NOT NULL,
+    [Name]	            nvarchar(350)	    NULL,
+    [EducationalType]	nvarchar(150)	    NULL,
+    [AddressLine1]		nvarchar(150)	    NULL,
+    [AddressLine2]		nvarchar(150)	    NULL,
+    [AddressLine3]		nvarchar(150)	    NULL,
+    [Town]			    nvarchar(50)	    NULL,
+    [County]			nvarchar(50)	    NULL,
+    [PostCode]			nvarchar(8)		    NULL,
+    [URN]	            nvarchar(100)		NULL,
+    CONSTRAINT [PK_EducationalOrganisation_Import] PRIMARY KEY (Id)
+)
--- src/SFA.DAS.EducationalOrganisations.Database/Tables/ImportAudit.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[ImportAudit]
-(
-	[Id] INT PRIMARY KEY IDENTITY(1,1),
-	[TimeStarted] DATETIME NOT NULL,
-	[TimeFinished] DATETIME NOT NULL,
-	[RowsImported] INT NOT NULL
-)
-GO
